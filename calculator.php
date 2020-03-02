1fb8dd1a30b0bc0ca1fa898a24385b68e0b0cea6
HENRY BENAYE <2106747@talnet.nl>
2020-03-02 13:36:01 +0100

    Uitwerking van de exercise

---
+++ calculator.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Calculator</h1>
+    <form action="" method="get">
+        <div>
+            <input type="number" name="nummer1">
+            <label for="">First Number</label>
+        </div>
+        
+        <div>
+            <input type="number" name="nummer2" style="margin-top: 20px">
+            <label for="">Second Number</label>
+        </div>
+        <?php
+            $antwoord;
+            $num1 = $_GET["nummer1"];
+            $num2 = $_GET["nummer2"];
+            
+            
+            switch($_GET["operation"]) {
+                case "Add":
+                    $antwoord = $num1 + $num2;
+                    break;
+                case "Subtract":
+                    $antwoord = $num1 - $num2;
+                    break;
+                case "Multiply":
+                    $antwoord = $num1 * $num2;
+                    break;
+                case "Divide":
+                    $antwoord = $num1 / $num2;
+                    break;
+                case "Modulo":
+                    $antwoord = $num1 * $num2;
+                    break;
+
+
+            }
+            ?>
+        <?php echo$antwoord; ?>
+        
+        <input type="submit" value="Add" style="margin-top: 20px" name="operation">
+        <input type="submit" value="Subtract" name="operation">
+        <input type="submit" value="Multiply" name="operation">
+        <input type="submit" value="Divide" name="operation">
+        <input type="submit" value="Modulo" name="operation">
+
+    </form>
+
+</body>
+
+</html>
