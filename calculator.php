52b32f5683aa84793c14f51c35fbe8266031a302
HENRY BENAYE <2106747@talnet.nl>
2020-03-02 13:52:53 +0100

    Uitwerking van de exercise

--- calculator.php
+++ calculator.php
@@ -14,39 +14,37 @@
             <input type="number" name="nummer1">
             <label for="">First Number</label>
         </div>
-        
+
         <div>
             <input type="number" name="nummer2" style="margin-top: 20px">
             <label for="">Second Number</label>
         </div>
         <?php
-            $antwoord;
-            $num1 = $_GET["nummer1"];
-            $num2 = $_GET["nummer2"] ?? null;
-            
-            
-            switch($_GET["operation"]) {
-                case "Add":
-                    $antwoord = $num1 + $num2;
-                    break;
-                case "Subtract":
-                    $antwoord = $num1 - $num2;
-                    break;
-                case "Multiply":
-                    $antwoord = $num1 * $num2;
-                    break;
-                case "Divide":
-                    $antwoord = $num1 / $num2;
-                    break;
-                case "Modulo":
-                    $antwoord = $num1 * $num2;
-                    break;
+        $antwoord;
+        $num1 = $_GET["nummer1"];
+        $num2 = $_GET["nummer2"] ?? null;
 
 
-            }
-            ?>
-        <?php echo$antwoord; ?>
-        
+        switch ($_GET["operation"]) {
+            case "Add":
+                $antwoord = $num1 + $num2;
+                break;
+            case "Subtract":
+                $antwoord = $num1 - $num2;
+                break;
+            case "Multiply":
+                $antwoord = $num1 * $num2;
+                break;
+            case "Divide":
+                $antwoord = $num1 / $num2;
+                break;
+            case "Modulo":
+                $antwoord = $num1 * $num2;
+                break;
+        }
+        ?>
+        <?php echo $antwoord; ?>
+
         <input type="submit" value="Add" style="margin-top: 20px" name="operation">
         <input type="submit" value="Subtract" name="operation">
         <input type="submit" value="Multiply" name="operation">
